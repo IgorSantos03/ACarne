c0db28aaf913c36182ae389d75caba4fdb085021
IgorSantos03 <gamerfewigor@gmail.com>
2023-11-30 15:35:20 -0300

    .

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,34 @@
-function mostrar(){
-    html=``;
-    html=`
-    <div><a href="autores.html"><Button class="bt-link">Autor</Button></a></div>
-    <div><a href="resumo.html"><button class="bt-link">Resumo</button></a></div>
-    <div><select name="cartas" id="cartas" onchange="location.href=this.value" class="bt-link">
-            <option value="">Cartas</option>
-            <option value="">Carta1</option>
-            <option value="">Carta2</option>
-        </select></div>
-    <div><a href="autores.html"><button class="bt-link">Alunos</button></a></div>
+function mostrar() {
+    html = ``;
+    html = `
+    <div class="foto">
+        <div><a href="autores.html"><Button class="bt-link">Autor</Button></a></div>
+        <div><a href="resumo.html"><button class="bt-link">Resumo</button></a></div>
+        <div><select name="cartas" id="cartas" onchange="location.href=this.value" class="bt-link">
+                <option value="">Cartas</option>
+                <option value="">Carta1</option>
+                <option value="">Carta2</option>
+            </select></div>
+        <div><a href="autores.html"><button class="bt-link">Alunos</button></a></div>
+    </div>
     `
     document.getElementById("menu").innerHTML = html;
     mudar();
 }
 
-function mudar(){
-    
-    var html1=``;
-    var html1=`
-    <button type="button" id="bt-sair" onclick="limpa()"><img src="https://cdn-icons-png.flaticon.com/256/3031/3031157.png"/></button>
-    `   
-    document.getElementById("mostra").innerHTML=html1;
+function mudar() {
+
+    var html1 = ``;
+    var html1 = `
+    <button type="button" id="bt-sair" onclick="limpa()" class="bt-mostar"><img class="icon" src="https://cdn-icons-png.flaticon.com/512/109/109750.png"/></button>
+    `
+    document.getElementById("mostra").innerHTML = html1;
 }
 
-function limpa(){
+function limpa() {
     window.location.reload(true);
 }
 
-document.getElementById('bt-mostrar').addEventListener('click', function(event){
+document.getElementById('bt-mostrar').addEventListener('click', function (event) {
     mostrar();
 });

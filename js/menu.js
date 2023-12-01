11d2dd696f1bfbddc700cda944c0c82994414ace
IgorSantos03 <gamerfewigor@gmail.com>
2023-12-01 15:08:23 -0300

    tentando utilizar bootstrap

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ function mostrar() {
     html = `
         <div><a href="autores.html"><Button class="bt-link">Autor</Button></a></div>
         <div><a href="resumo.html"><button class="bt-link">Resumo</button></a></div>
+        <div><a href="professores.html"><button class="bt-link">professores</button></a></div>
         <div><select name="cartas" id="cartas" onchange="location.href=this.value" class="bt-link">
                 <option value="">Cartas</option>
                 <option value="">Carta1</option>
